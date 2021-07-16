8a07079ef111c2bb96c563629df85a23c35077e7
raviprasad2212 <raviprasad2545@gmail.com>
2021-07-16 13:35:00 +0530

    added max_age

--- College/CSEbranch/schema.py
+++ College/CSEbranch/schema.py
@@ -1,10 +1,11 @@
-from graphene import ObjectType, Float, Field
+from graphene import ObjectType, Float, Field, Int
 from django.db.models import Avg, Max
 from .models import Students
 
 # Mention to print data in the Graphql server
 class Student_avg_age(ObjectType):
     averageAGeStudent = Float()
+    maxage = Int()
 
 # Actual method implementation in class
 class ShowAvgAge(ObjectType):
@@ -13,4 +14,5 @@ class ShowAvgAge(ObjectType):
 
     def resolve_showavg(self, info):
         get_avg_age = Students.objects.aggregate(Avg('age'))
-        return Student_avg_age(averageAGeStudent=get_avg_age.get('age__avg'))
+        max_age = Students.objects.aggregate(Max('age'))
+        return Student_avg_age(averageAGeStudent=get_avg_age.get('age__avg'), maxage = max_age.get('age__max'))
